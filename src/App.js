3ed2823a16b6972b6b2d32f73649dc9080c9ed28
hawkesie <jordanhawkes93@gmail.com>
2023-03-10 18:48:15 +0800

    redesign first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 
 import './App.css';
 import { useEffect, useRef, useState } from "react";
-import Header from './components/Header/Header';
-import MySites from './components/MySites/MySites';
-import MyGits from './components/MyGits/MyGits';
-import MyEducation from './components/MyEducation/MyEducation';
-import Contact from './components/Contact/Contact';
+import Header from './containers/Header/Header'
+
 
 function App() {
 
   return (
     <div className="App">
-      
-      
-
       <div className='main-container'>
         <Header />
-        <MySites />
-       
-        <MyEducation />
-         <MyGits/>
-        <Contact />
-        </div>
       </div>
+    </div>
   );
 }
 
