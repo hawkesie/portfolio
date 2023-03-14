12d6e7b18340004647045fed593eea5a079b5042
hawkesie <jordanhawkes93@gmail.com>
2023-03-14 11:42:52 +0800

    beginning of portfolio section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import { useEffect, useRef, useState } from "react";
 import Header from './containers/Header/Header'
+import Portfolio from './containers/Portfolio/Portfolio';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <div className='main-container'>
         <Header />
+        <Portfolio />
         <div className='bottom'></div>
       </div>
     </div>
---
+++ src/containers/Portfolio/Portfolio.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Portfolio.css'
+
+const Portfolio = () => {
+  return (
+    <div className='portfolio-container'>
+        <div className='portfolio-top'>
+            <h4>My Portfolio</h4>
+            <h1>Creative Meets Technology</h1>
+        </div>
+        <div className='portfolio-main-container'>
+            <div className='portfolio-main-left'>
+                <h2>Geisha NFT</h2>
+                <p>I was hired to build this landing page for a potential NFT Project. I love the art on this page, which is why I haven't...</p>
+                <button>View Project</button>
+            </div>
+            <div className='portfolio-main-right'>
+                
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Portfolio
